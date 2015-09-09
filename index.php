0c162fe0c555ba7058fd1d011523f96860791681
supra.cn <supra.cn@gmail.com>
2015-09-09 10:59:53 +0800

    add donate at index

--- index.php
+++ index.php
@@ -13,7 +13,8 @@ include_once 'header.php';
         <p class="lead">交流学习q群：485561913。</p>
         <p class="lead">联系站长请在<a href="http://blog.supra.tw">Supra's Garage</a>留言;</p>
         <p class="lead">或mailto:<a href="mailto:supra.cn@gmail.com">Supra.CN@Gmail.com</a>。</p>
-        <p><a class="btn btn-lg btn-success" href="user/register.php" role="button">立即注册</a></p>
+        <p><a class="btn btn-lg btn-success" href="user/register.php" role="button">立即注册</a>
+	<a class="btn btn-lg btn-success" href="http://donate.supra.tw" role="button">捐助Supra</a></p>
     </div>
 
 
